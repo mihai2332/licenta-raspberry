d813d60b9d598777580682f43d6e04fea11884fe
mihai ciurca <Mihai.Ciurca@orange.com>
2019-03-02 18:22:03 +0200

    keep alive and name for jar changed

--- src/main/java/com/mimihaisuper/licenta/config/MqttConfiguration.java
+++ src/main/java/com/mimihaisuper/licenta/config/MqttConfiguration.java
@@ -3,6 +3,7 @@ package com.mimihaisuper.licenta.config;
 import com.mimihaisuper.licenta.services.PersistenceService;
 import org.eclipse.paho.client.mqttv3.IMqttClient;
 import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -18,7 +19,9 @@ public class MqttConfiguration {
     @Bean
     public void getPing() throws MqttException {
         IMqttClient mqttClient = new MqttClient("tcp://localhost:1883", "RaspPi");
-        mqttClient.connect();
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setKeepAliveInterval(86400);
+        mqttClient.connect(options);
         mqttClient.subscribe(TOPIC, (TOPIC, msg) -> {
             String[] data = TOPIC.split("/");
             persistenceService.postSensorMessage(msg.toString(), data[1]);
