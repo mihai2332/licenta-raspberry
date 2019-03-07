480be0d90dd30da33c7ea9e72eb10e2200c88737
mihai ciurca <Mihai.Ciurca@orange.com>
2019-03-07 15:14:52 +0200

    added logging for debugging

--- src/main/java/com/mimihaisuper/licenta/config/MqttConfiguration.java
+++ src/main/java/com/mimihaisuper/licenta/config/MqttConfiguration.java
@@ -5,6 +5,8 @@ import org.eclipse.paho.client.mqttv3.IMqttClient;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +14,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MqttConfiguration {
     private static final String TOPIC = "esp/#";
+    Logger logger = LoggerFactory.getLogger(MqttConfiguration.class);
 
     @Autowired
     PersistenceService persistenceService;
@@ -24,6 +27,7 @@ public class MqttConfiguration {
         mqttClient.connect(options);
         mqttClient.subscribe(TOPIC, (TOPIC, msg) -> {
             String[] data = TOPIC.split("/");
+            logger.info("message "+ msg.toString() + " received!");
             persistenceService.postSensorMessage(msg.toString(), data[1]);
         });
     }
