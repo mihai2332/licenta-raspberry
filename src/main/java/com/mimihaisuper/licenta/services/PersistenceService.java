9e232288343a7da919f499c606a3b360d0488d19
mihai ciurca <Mihai.Ciurca@orange.com>
2019-03-07 15:15:21 +0200

    added logging for debugging

--- src/main/java/com/mimihaisuper/licenta/services/PersistenceService.java
+++ src/main/java/com/mimihaisuper/licenta/services/PersistenceService.java
@@ -2,6 +2,8 @@ package com.mimihaisuper.licenta.services;
 
 import com.mimihaisuper.licenta.models.Sensor;
 import com.mimihaisuper.licenta.repository.SensorRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +11,7 @@ import java.util.Date;
 
 @Service
 public class PersistenceService {
+    Logger logger = LoggerFactory.getLogger(PersistenceService.class);
 
     @Autowired
     SensorRepository sensorRepository;
@@ -19,6 +22,6 @@ public class PersistenceService {
         sensor.setValue(value);
         sensor.setSensorType(sensorType);
         sensorRepository.save(sensor);
-        System.out.println("message " + value + " posted!");
+        logger.info("message " + value + " posted!");
     }
 }
